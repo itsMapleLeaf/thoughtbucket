2b2c6129a2521dc5f1b00afbcb0ef531f3f860bd
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-16 17:34:51 -0500

    add loading icon

---
+++ src/modules/ui/LoadingIcon.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx"
+import React from "react"
+import css from "./LoadingIcon.module.css"
+
+export function LoadingIcon({ size = 4 }: { size?: 2 | 4 | 6 }) {
+  const dotSizeClass = {
+    2: clsx`w-2 h-2`,
+    4: clsx`w-4 h-4`,
+    6: clsx`w-6 h-6`,
+  }[size]
+
+  return (
+    <div
+      className={clsx(
+        "grid gap-2 place-content-center place-items-center grid-cols-2 grid-rows-2 drop-shadow",
+        size === 2 && "gap-1",
+        size === 4 && "gap-2",
+        size === 6 && "gap-4",
+        css.spin,
+      )}
+    >
+      <div className={clsx("bg-white rounded-full", dotSizeClass)}></div>
+      <div className={clsx("bg-green-500 rounded-full", dotSizeClass)}></div>
+      <div className={clsx("bg-green-500 rounded-full", dotSizeClass)}></div>
+      <div className={clsx("bg-white rounded-full", dotSizeClass)}></div>
+    </div>
+  )
+}
