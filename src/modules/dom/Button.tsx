3a99ca913199b4cd18ea2b3ccfddfd21bc4c288c
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-16 17:35:01 -0500

    add loading prop on button

--- src/modules/dom/Button.tsx
+++ src/modules/dom/Button.tsx
@@ -1,10 +1,19 @@
 import clsx from "clsx"
 import type { ComponentPropsWithoutRef } from "react"
 import React from "react"
+import { LoadingIcon } from "../ui/LoadingIcon"
 
-export type ButtonProps = ComponentPropsWithoutRef<"button">
+export type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  loading?: unknown
+}
 
-export function Button(props: ButtonProps) {
+export function Button({
+  loading,
+  className,
+  disabled,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       type="button"
@@ -12,14 +21,16 @@ export function Button(props: ButtonProps) {
       // alternate, more accessible disabled behavior
       disabled={false}
       className={clsx(
-        props.disabled && "opacity-75 pointer-events-none cursor-not-allowed",
-        props.className,
+        className,
+        (Boolean(loading) || disabled) &&
+          "opacity-75 pointer-events-none cursor-not-allowed",
       )}
       onClick={(event) => {
-        if (!props.disabled) {
-          props.onClick?.(event)
-        }
+        if (disabled || loading) return
+        props.onClick?.(event)
       }}
-    />
+    >
+      {loading ? <LoadingIcon size={2} /> : children}
+    </button>
   )
 }
