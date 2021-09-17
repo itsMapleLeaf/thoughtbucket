a43269a3dcffe5e010831d149f717daa6eade1f3
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-16 20:39:59 -0500

    add solid danger button class

--- src/modules/ui/button.ts
+++ src/modules/ui/button.ts
@@ -1,10 +1,25 @@
 import clsx from "clsx"
 import { activePressClass } from "./effects"
 
+const solidButtonBaseClass = clsx`
+  px-3 py-2
+  rounded
+  leading-none text-shadow text-white
+  shadow
+  transition
+  font-semibold
+`
+
 export const solidButtonClass = clsx`
-  px-3 py-2 rounded leading-none shadow transition font-semibold text-shadow
-  text-white bg-green-700 hover:bg-green-800
+  ${solidButtonBaseClass}
   ${activePressClass}
+ bg-green-700 hover:bg-green-800
+`
+
+export const solidDangerButtonClass = clsx`
+  ${solidButtonBaseClass}
+  ${activePressClass}
+  bg-red-700 hover:bg-red-800
 `
 
 export const fadedButtonClass = clsx`
