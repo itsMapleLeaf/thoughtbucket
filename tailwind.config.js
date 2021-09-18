506c390487ac32826150d0e11b0ae8fbbab44607
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-17 20:32:42 -0500

    typecheck tailwind config?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const colors = require("tailwindcss/colors")
 
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{ts,tsx}"],
@@ -17,11 +18,13 @@ module.exports = {
           "0 1px 4px 0 rgba(0, 0, 0, 0.25), 0 1px 2px 0 rgba(0, 0, 0, 0.18)",
         inner: "inset 0 1px 4px 0 rgba(0, 0, 0, 0.25)",
       },
+      // @ts-expect-error
       minWidth: (theme) => theme("maxWidth"),
     },
   },
   variants: {
     extend: {},
   },
+  // @ts-expect-error
   plugins: [require("@tailwindcss/line-clamp")],
 }
