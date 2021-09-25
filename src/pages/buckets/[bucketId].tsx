6da5b3d1da95a62dc2e8b3751ea92375a3b43cab
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-24 21:34:24 -0500

    go back to inline patch for bucket column updates

--- src/pages/buckets/[bucketId].tsx
+++ src/pages/buckets/[bucketId].tsx
@@ -1,17 +1,15 @@
 import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid"
 import type { User } from "@prisma/client"
-import type { Draft } from "immer"
 import produce from "immer"
 import { nanoid } from "nanoid"
-import { handle, json, redirect } from "next-runtime"
+import { handle, json, notFound, redirect } from "next-runtime"
 import { useMemo, useRef, useState } from "react"
 import { z } from "zod"
 import { getClient } from "../../db/client"
+import { createSessionHelpers } from "../../db/session"
 import { pick } from "../../helpers"
+import { httpCodes } from "../../http-codes"
 import { AppLayout } from "../../modules/app/AppLayout"
-import { usingSessionUser } from "../../modules/auth/usingSessionUser"
-import type { BucketData } from "../../modules/bucket/BucketData"
-import { bucketDataSchema } from "../../modules/bucket/BucketData"
 import { BucketPageSummary } from "../../modules/bucket/BucketPageSummary"
 import type { ClientBucket } from "../../modules/bucket/ClientBucket"
 import { asClientBucket } from "../../modules/bucket/ClientBucket"
@@ -20,7 +18,7 @@ import { usingBucket } from "../../modules/bucket/usingBucket"
 import { usingOwnedBucket } from "../../modules/bucket/usingOwnedBucket"
 import { ColumnCard } from "../../modules/column/ColumnCard"
 import { Button } from "../../modules/dom/Button"
-import { fetchWithRetry } from "../../modules/network/fetchWithRetry"
+import { fetchJsonWithRetry } from "../../modules/network/fetchWithRetry"
 import { getContextParam } from "../../modules/routing/getContextParam"
 import { useObservable } from "../../modules/rxjs/useObservable"
 import { ThoughtCard } from "../../modules/thought/ThoughtCard"
@@ -29,23 +27,39 @@ import { containerClass } from "../../modules/ui/container"
 import { leftButtonIconClass } from "../../modules/ui/icon"
 import { QuickInsertForm } from "../../modules/ui/QuickInsertForm"
 
+type BucketColumn = z.TypeOf<typeof bucketColumnSchema>
+const bucketColumnSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  thoughts: z.array(
+    z.object({
+      id: z.string(),
+      text: z.string(),
+    }),
+  ),
+})
+
 type Props = {
-  user: Pick<User, "name">
+  user?: Pick<User, "name">
   bucket: ClientBucket
-  bucketData: BucketData
+  columns: BucketColumn[]
   errorMessage?: string
 }
 
+const patchBodySchema = z.object({
+  name: z.string().optional(),
+  columns: z.array(bucketColumnSchema).optional(),
+})
+
 export const getServerSideProps = handle<Props>({
   get: async (context) => {
-    return usingSessionUser(context, (user) => {
-      const bucketId = getContextParam(context, "bucketId")
-      return usingBucket(bucketId, (bucket) => {
-        return json<Props>({
-          user: pick(user, ["name"]),
-          bucket: asClientBucket(bucket),
-          bucketData: bucketDataSchema.parse(JSON.parse(bucket.data)),
-        })
+    const user = await createSessionHelpers(context).getUser()
+    const bucketId = getContextParam(context, "bucketId")
+    return usingBucket(bucketId, (bucket) => {
+      return json<Props>({
+        user: user ? pick(user, ["name"]) : undefined,
+        bucket: asClientBucket(bucket),
+        columns: z.array(bucketColumnSchema).parse(bucket.columns),
       })
     })
   },
@@ -61,68 +75,76 @@ export const getServerSideProps = handle<Props>({
 
   patch: async (context) => {
     const db = getClient()
+    const body = patchBodySchema.parse(context.req.body)
     const bucketId = getContextParam(context, "bucketId")
 
-    const patchBodySchema = z
-      .object({
-        name: z.string(),
-        data: bucketDataSchema,
-      })
-      .partial()
-
-    const body = patchBodySchema.parse(context.req.body)
-
-    return usingOwnedBucket(context, bucketId, async (user) => {
-      await db.bucket.update({
-        where: { id: bucketId },
-        data: {
-          ...body,
-          data: body.data ? JSON.stringify(body.data) : undefined,
-        },
-      })
-
-      return redirect(context.req.url!, 303)
+    const bucket = await db.bucket.findUnique({
+      where: {
+        id: bucketId,
+      },
     })
+    if (!bucket) {
+      return notFound()
+    }
+
+    const user = await createSessionHelpers(context).getUser()
+
+    if (bucket.ownerId !== user?.id) {
+      return json(
+        {
+          bucket: asClientBucket(bucket),
+          columns: z.array(bucketColumnSchema).parse(bucket.columns),
+        },
+        httpCodes.forbidden,
+      )
+    }
+
+    await db.bucket.update({
+      where: {
+        id: bucketId,
+      },
+      data: {
+        name: body.name,
+        columns: body.columns,
+      },
+    })
+
+    return json(
+      {
+        user: pick(user, ["name"]),
+        bucket: asClientBucket(bucket),
+        columns: z.array(bucketColumnSchema).parse(bucket.columns),
+      },
+      httpCodes.ok,
+    )
   },
 })
 
 export default function BucketPage({
   user,
   bucket,
-  bucketData: initialBucketData,
+  columns: initialColumns,
 }: Props) {
-  const [bucketData, setBucketData] = useState(initialBucketData)
+  const [columns, setColumns] = useState(initialColumns)
 
   const createColumn = (name: string) =>
-    setBucketData(
-      produce((draft) => {
-        draft.columns.push({
-          id: nanoid(),
-          name,
-          thoughts: [],
-        })
-      }),
-    )
+    setColumns((columns) => [...columns, { id: nanoid(), name, thoughts: [] }])
 
   const deleteColumn = (id: string) =>
-    setBucketData(
-      produce((draft) => {
-        draft.columns = draft.columns.filter((c) => c.id !== id)
-      }),
-    )
+    setColumns((columns) => columns.filter((c) => c.id !== id))
 
   const createThought = (args: { columnId: string; text: string }) =>
-    setBucketData(
+    setColumns(
       produce((draft) => {
-        const column = draft.columns.find((c) => c.id === args.columnId)
+        const column = draft.find((c) => c.id === args.columnId)
         column?.thoughts.unshift({ id: nanoid(), text: args.text })
       }),
     )
 
   const deleteThought = (args: { columnId: string; thoughtId: string }) =>
-    setBucketData(
-      produce((draft: Draft<BucketData>) => {
-        const column = draft.columns.find((c) => c.id === args.columnId)
+    setColumns(
+      produce((draft) => {
+        const column = draft.find((c) => c.id === args.columnId)
         if (column) {
           column.thoughts = column.thoughts.filter(
             (t) => t.id !== args.thoughtId,
@@ -133,18 +155,14 @@ export default function BucketPage({
 
   const fetchState = useObservable(
     useMemo(() => {
-      if (bucketData === initialBucketData) return
+      if (columns === initialColumns) return
 
-      return fetchWithRetry(`/buckets/${bucket.id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          data: bucketData,
-        }),
+      return fetchJsonWithRetry({
+        url: window.location.pathname,
+        method: "patch",
+        data: { columns },
       })
-    }, [bucket.id, bucketData, initialBucketData]),
+    }, [columns, initialColumns]),
     { status: "init" as const },
   )
 
@@ -180,7 +198,7 @@ export default function BucketPage({
           className="grid grid-flow-col gap-4 p-4 auto-cols-[18rem] grid-rows-1 mx-auto min-w-[min(1024px,100%)] max-w-full overflow-auto flex-1"
           ref={columnScrollContainerRef}
         >
-          {bucketData.columns.map((column) => (
+          {columns.map((column) => (
             <ColumnCard key={column.id}>
               <ColumnCard.Header
                 title={column.name}
