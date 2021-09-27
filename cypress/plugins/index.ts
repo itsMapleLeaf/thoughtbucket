5a01b2ed63854aa0c9c41583cda301cf199a9332
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-26 19:54:19 -0500

    add cypress esbuild processor

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,2 +1,16 @@
-const pluginConfig: Cypress.PluginConfig = (on, config) => {}
+import { cypressEsbuildPreprocessor } from "cypress-esbuild-preprocessor"
+import path = require("path")
+
+const pluginConfig: Cypress.PluginConfig = (on, config) => {
+  on(
+    "file:preprocessor",
+    cypressEsbuildPreprocessor({
+      esbuildOptions: {
+        // optional tsconfig for typescript support and path mapping (see https://github.com/evanw/esbuild for all options)
+        tsconfig: path.resolve(__dirname, "../tsconfig.json"),
+      },
+    }),
+  )
+}
+
 export = pluginConfig
