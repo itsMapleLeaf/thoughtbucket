a20de8d13aac99b7de389f59b81983b6b415a778
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-10-03 21:30:58 -0500

    trim tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,18 @@
-const colors = require("tailwindcss/colors")
-
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: `'Nunito', sans-serif`,
+    },
+    boxShadow: {
+      DEFAULT:
+        "0 1px 4px 0 rgba(0, 0, 0, 0.25), 0 1px 2px 0 rgba(0, 0, 0, 0.18)",
+      inner: "inset 0 1px 4px 0 rgba(0, 0, 0, 0.25)",
+    },
     extend: {
-      colors: {
-        gray: colors.slate,
-      },
-      fontFamily: {
-        sans: `'Nunito', sans-serif`,
-      },
-      boxShadow: {
-        DEFAULT:
-          "0 1px 4px 0 rgba(0, 0, 0, 0.25), 0 1px 2px 0 rgba(0, 0, 0, 0.18)",
-        inner: "inset 0 1px 4px 0 rgba(0, 0, 0, 0.25)",
-      },
-      // @ts-expect-error
       minWidth: (theme) => theme("maxWidth"),
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/line-clamp")],
 }
