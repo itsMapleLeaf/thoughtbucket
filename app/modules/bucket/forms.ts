8ed31a6dbc04abfcdc4a42212ae5c426ceeaa8c7
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-14 16:43:30 -0600

    add EditBucketForm + body type

--- app/modules/bucket/forms.ts
+++ app/modules/bucket/forms.ts
@@ -1,4 +1,5 @@
 import { z } from "zod"
+import { columnListSchema } from "~/modules/column/Column"
 import { createFormHelpers } from "~/modules/remix/form"
 
 export const CreateBucketForm = createFormHelpers(
@@ -6,3 +7,10 @@ export const CreateBucketForm = createFormHelpers(
     name: z.string(),
   }),
 )
+
+export type EditBucketBody = z.TypeOf<typeof editBucketBodySchema>
+const editBucketBodySchema = z.object({
+  name: z.string().optional(),
+  columns: columnListSchema.optional(),
+})
+export const EditBucketForm = createFormHelpers(editBucketBodySchema)
