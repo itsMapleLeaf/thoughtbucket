26eb3eb20cc3851860200440e4234ab28f495bca
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-14 16:46:16 -0600

    allowedMethods: always returns lowercase method

--- app/modules/remix/allow-methods.ts
+++ app/modules/remix/allow-methods.ts
@@ -2,18 +2,19 @@ import { httpCodes } from "~/modules/network/http-codes"
 import { responseTyped } from "~/modules/remix/data"
 
 type HttpMethodUpper = "GET" | "POST" | "PATCH" | "DELETE" | "PUT"
-type HttpMethod = HttpMethodUpper | Lowercase<HttpMethodUpper>
+type HttpMethodLower = Lowercase<HttpMethodUpper>
+type HttpMethod = HttpMethodUpper | HttpMethodLower
 
 export function allowMethods<AllowedMethod extends HttpMethod>(
   request: Request,
   methods: AllowedMethod[],
-): AllowedMethod {
-  const method = request.method.toLowerCase() as HttpMethod
+): Lowercase<AllowedMethod> {
+  const method = request.method.toLowerCase() as Lowercase<AllowedMethod>
 
   const isAllowed = methods.map((m) => m.toLowerCase()).includes(method)
   if (!isAllowed) {
     throw responseTyped(undefined, httpCodes.methodNotAllowed)
   }
 
-  return method as AllowedMethod
+  return method
 }
