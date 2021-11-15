7c93bb8557d2764fd36dd64996c92029d5d00fa3
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-15 02:57:48 -0600

    fix DateTime type error

--- app/modules/dom/DateTime.tsx
+++ app/modules/dom/DateTime.tsx
@@ -1,9 +1,13 @@
-import React from "react"
+type DateTimeProps = Intl.DateTimeFormatOptions & {
+  date: string | number | Date
 
-export function DateTime({
-  date,
-  ...options
-}: { date: string | number | Date } & Intl.DateTimeFormatOptions) {
+  // these properties used to exist in the typedefs, but they got yeeted for some reason
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#parameters
+  dateStyle?: "full" | "long" | "medium" | "short"
+  timeStyle?: "full" | "long" | "medium" | "short"
+}
+
+export function DateTime({ date, ...options }: DateTimeProps) {
   return (
     <time dateTime={new Date(date).toISOString()}>
       {new Date(date).toLocaleString(undefined, options)}
